c606ffb66b17bb9de2206e80037591965778da08
Dominik Kopec <kopec.dominik@gmail.com>
2022-10-17 14:04:52 -0400

    feat: update, upgrade, install gh-cli

--- src/wsl-init.sh
+++ src/wsl-init.sh
@@ -4,4 +4,16 @@
 #
 
 # Add the ubunut repo sources to apt
-apt-add-repository http://archive.ubuntu.com/ubuntu/
+sudo apt-add-repository http://archive.ubuntu.com/ubuntu/
+
+# Update and Upgrade
+sudo apt update \
+&& sudo apt upgrade
+
+# Install github cli https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
