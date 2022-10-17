a22bf9ffc40f2b6256c2a1b163d366937a65f2a2
Dominik Kopec <kopec.dominik@gmail.com>
2022-10-17 13:52:03 -0400

    feat: add ubuntu sources

---
+++ src/wsl-init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Script representing the manual init steps for setting up my dev instance.
+#
+
+# Add the ubunut repo sources to apt
+apt-add-repository http://archive.ubuntu.com/ubuntu/
